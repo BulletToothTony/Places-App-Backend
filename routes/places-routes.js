a99e9b1372c40c26ac41a5c6c293962893d57c87
Henry <henrywl@gmail.com>
2024-04-10 15:57:51 +0100

    error handling

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -14,29 +14,6 @@ const DUMMY_PLACES = [
     address: "20 W 34th St, New York, NY 10001",
     creator: "u1",
   },
-  {
-    id: "p2",
-    title: "White house",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u2",
-  },
-  {
-    id: "p3",
-    title: "omg building 3",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  }
-
 ];
 
 router.get("/:pid", (req, res, next) => {
@@ -44,17 +21,32 @@ router.get("/:pid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.')
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place });
 });
 
-// getting list of places from user ID
 router.get("/user/:uid", (req, res, next) => {
-    const userId = req.params.uid
-    console.log(userId)
-    const userPlaces = DUMMY_PLACES.filter((p) => {
-        return p.creator === userId
-    })
-    res.json(userPlaces)
-})
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.filter((p) => {
+    return p.creator === userId;
+  });
+
+  console.log(place.length)
+
+// below line 40 doesn't match code from guide, instead they use if (!place) {} may need to change back in future
+  if (place.length === 0) {
+    const error = new Error('Could not find a place for the provided user id.')
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({place});
+});
 
 module.exports = router;
