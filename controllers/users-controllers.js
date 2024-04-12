0edc8d153ee240680f011647af493eb23ecf802a
Henry <henrywl@gmail.com>
2024-04-12 01:33:23 +0100

    signup validation

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const uuid = require("uuid").v4;
 
+const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 
 const DUMMY_USERS = [
@@ -16,15 +17,19 @@ const getUsers = (req, res, next) => {
 };
 
 const signup = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+  
   const { name, email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email)
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
 
   if (hasUser) {
-    throw new HttpError(
-        "Could not create user, email already exists",
-        422
-      );
+    throw new HttpError("Could not create user, email already exists", 422);
   }
 
   const createdUser = {
